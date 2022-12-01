d57c24a365b68d8bc999d677766e7b5c7d8ffcbd
KellyMark <kmark602@gmail.com>
2022-12-01 15:25:33 -0500

    Created files for about section and game section of website

---
+++ script.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("#background");
+let bodyBackground = body.style.background;
+const randomColors = ["cadetblue","purple", "blue", "red", "pink", "yellow", "green", "cyan", "white", "orange", "brown"];
+let currentIndex = randomColors[bodyBackground];
+body.addEventListener("click", ChangeBackgroundColor)
+
+function createRandomIndex(currentIndex) {
+    const numColors = randomColors.length;
+    let index = Math.floor(Math.random()*numColors);
+    while (index === currentIndex){
+        index = Math.floor(Math.random()*numColors);
+    }
+    return index;
+}
+function ChangeBackgroundColor() {
+    const nextIndex = createRandomIndex(currentIndex);
+    body.style.background = randomColors[nextIndex];
+    currentIndex = nextIndex;
+}
